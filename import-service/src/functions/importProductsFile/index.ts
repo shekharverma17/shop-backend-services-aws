48da4d9bfdf7562a59b6aa3433ffbc0c5ecea7b9
Shekhar Verma <shekhar17verma@gmail.com>
2022-08-29 00:15:59 +0530

    task-7 changes

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -7,6 +7,12 @@ export default {
       http: {
         method: 'get',
         path: '/import',
+        authorizer: {
+          arn: 'arn:aws:lambda:us-east-1:003374823954:function:authorization-service-dev-basicAuthorizer',
+          name: 'basicAuthorizer',
+          type: 'TOKEN',
+          identitySource: 'method.request.header.Authorization',
+        },
       },
     },
   ],
